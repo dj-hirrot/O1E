780d7d70edc5c3325081ed6082de006394a97dcb
dj-hirrot <hirotoshibutani@gmail.com>
2020-03-11 18:15:33 +0900

    removed: 不要なファイル削除

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -2,7 +2,12 @@ require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
   def setup
-    @user = User.new(name: 'username', email: 'user@mail.com')
+    @user = User.new(
+      name: 'username',
+      email: 'user@mail.com',
+      password: 'password',
+      password_confirmation: 'password'
+    )
   end
 
   test "should be valid" do
@@ -66,9 +71,19 @@ class UserTest < ActiveSupport::TestCase
   end
 
   test "email should be saved as lower-case" do
-    mixed_case_email = "User@MaIl.CoM"
+    mixed_case_email = "Users@MaIl.CoM"
     @user.email = mixed_case_email
     @user.save
     assert_equal mixed_case_email.downcase, @user.reload.email
+  end
+
+  test "password should be present (nonblank)" do
+    @user.password = @user.password_confirmation = " " * 6
+    assert_not @user.valid?
+  end
+
+  test "password should have a minimum length" do
+    @user.password = @user.password_confirmation = "a" * 5
+    assert_not @user.valid?
   end
 end
