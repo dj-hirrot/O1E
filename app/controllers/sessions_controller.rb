d879913d5f940effc03c9869ae9aa402e91e38fa
dj-hirrot <hirotoshibutani@gmail.com>
2020-03-12 14:10:39 +0900

    update: ログアウト

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,12 +7,18 @@ class SessionsController < ApplicationController
     @user = User.find_by(email: params[:session][:email].downcase)
 
     if @user && @user.authenticate(params[:session][:password])
-      login(@user)
+      login @user
       flash[:success] = 'ログインに成功しました'
       redirect_to root_url
     else
-      flash.now[:danger] =  "メールアドレスかパスワードが間違っています"
+      flash.now[:danger] =  'メールアドレスかパスワードが間違っています'
       render :new
     end
+  end
+
+  def destroy
+    logout
+    flash[:success] = 'ログアウトしました'
+    redirect_to root_url
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
+      login @user
       flash[:success] = "確認用メールを送信しました"
       redirect_to root_path
     else
