fd2d1325954c392d785ecf26bbdbc51b7ee8175a
dj-hirrot <hirotoshibutani@gmail.com>
2020-03-12 17:48:12 +0900

    update: ユーザー有効化

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,25 @@
 Rails.application.routes.draw do
-  get 'users/:name', to: 'users#show', as: 'user'
-  get 'users/:name/edit', to: 'users#edit', as: 'edit_user'
-  patch 'users/:name', to: 'users#update', as: 'users'
-  get '/join', to: 'users#new', as: 'join'
-  post '/join', to: 'users#create', as: 'joins'
+  ###
+  ### Session
+  ###
   get '/login', to: 'sessions#new', as: 'login'
   post '/login', to: 'sessions#create', as: 'logins'
   delete '/logout', to: 'sessions#destroy', as: 'logout'
 
+  ###
+  ### User
+  ###
+  resources :users, only: [:show, :new, :create, :edit, :update], param: :name, as: :user
+  get '/join', to: 'users#new', as: 'join'
+  post '/join', to: 'users#create', as: 'joins'
+
+  ###
+  ### AccountActivation
+  ###
+  resources :account_activations, only: [:edit]
+
+  ###
+  ### Root
+  ###
   root to: 'home#index'
 end
