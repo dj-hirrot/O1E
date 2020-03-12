41fc8bd7b3c9f5da3d50fc0de017fdf209fe17af
dj-hirrot <hirotoshibutani@gmail.com>
2020-03-12 12:34:58 +0900

    update: ログイン

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
-  resources :users, only: [:new, :create]
+  get '/join', to: 'users#new', as: 'join'
+  post '/join', to: 'users#create', as: 'joins'
+  get '/login', to: 'sessions#new', as: 'login'
+  post '/login', to: 'sessions#create', as: 'logins'
+  delete '/logout', to: 'sessions#destroy', as: 'logout'
+
   root to: 'home#index'
 end
