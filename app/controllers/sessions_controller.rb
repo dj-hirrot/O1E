f24746a5e4a6d1bcff4923009c07283c747db435
dj-hirrot <hirotoshibutani@gmail.com>
2020-03-12 13:30:34 +0900

    update: viewの調整

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,17 @@
 class SessionsController < ApplicationController
   def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.find_by(email: params[:session][:email].downcase)
+
+    if @user && @user.authenticate(params[:session][:password])
+      flash.now[:success] =  "success"
+      render :new
+    else
+      flash.now[:danger] =  "メールアドレスかパスワードが間違っています"
+      render :new
+    end
   end
 end
