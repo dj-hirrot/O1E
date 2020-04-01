b527c6b0ee133ad1145b3ea57098e006c7b72bcb
dj-hirrot <hirotoshibutani@gmail.com>
2020-04-01 15:48:09 +0900

    update: フォーム送信

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,22 @@
 class TasksController < ApplicationController
+  before_action :set_subject
+
+  def create
+    @task = @subject.tasks.build(user_id: current_user.id)
+
+    if @task.update(task_params)
+      render json: { task: @task }, status: 200
+    else
+      render json: { task: @task.errors.full_messages }, status: 422
+    end
+  end
+
+  private
+    def set_subject
+      @subject = Subject.find(params[:subject_id])
+    end
+
+    def task_params
+      params.require(:task).permit(:name, :description)
+    end
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,7 @@
 class Task < ApplicationRecord
   belongs_to :subject
   belongs_to :user
+
+  validates :name, presence: true
+  validates :description, presence: true
 end
