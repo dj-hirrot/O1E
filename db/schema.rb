d347d4ba7e7a71affe140d991bddff6d88196c16
dj-hirrot <hirotoshibutani@gmail.com>
2020-04-08 00:51:18 +0900

    update: データベースの変更

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 
 ActiveRecord::Schema.define(version: 2020_03_31_145018) do
 
-  create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
+  create_table "categories", force: :cascade do |t|
     t.string "code"
     t.string "name"
     t.text "description"
@@ -22,9 +22,9 @@ ActiveRecord::Schema.define(version: 2020_03_31_145018) do
     t.index ["name"], name: "index_categories_on_name", unique: true
   end
 
-  create_table "subjects", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
-    t.bigint "user_id", null: false
-    t.bigint "category_id", null: false
+  create_table "subjects", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "category_id", null: false
     t.string "name"
     t.text "description"
     t.integer "progress", default: 0
@@ -34,19 +34,19 @@ ActiveRecord::Schema.define(version: 2020_03_31_145018) do
     t.index ["user_id"], name: "index_subjects_on_user_id"
   end
 
-  create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
-    t.bigint "subject_id", null: false
-    t.bigint "user_id", null: false
-    t.string "name"
-    t.text "description"
-    t.boolean "done"
+  create_table "tasks", force: :cascade do |t|
+    t.integer "subject_id", null: false
+    t.integer "user_id", null: false
+    t.string "name", null: false
+    t.text "description", null: false
+    t.boolean "done", default: false, null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["subject_id"], name: "index_tasks_on_subject_id"
     t.index ["user_id"], name: "index_tasks_on_user_id"
   end
 
-  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
+  create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.string "password_digest"
